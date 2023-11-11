25f50a7b48432f4e7761c8a1f6ce1f0a16189293
Boris Korobeinikov <slon2k@gmail.com>
2023-11-11 22:11:41 +0600

    Navbar and Sidebar components created

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { AiOutlineLogout } from 'react-icons/ai'
+import { BiSearch } from 'react-icons/bi'
+import { IoMdAdd } from 'react-icons/io'
+import Logo from '../public/tiktik-logo.png'
+
 export const Navbar: React.FC = () => {
   return (
-    <div>
-      Navbar component
+    <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+      <Link href='/'>
+        <div className='w-[100px] md:w-[130px]'>
+          <div>
+            <Image className='cursor-pointer' src={Logo} alt='TikTik' layout='responsive' />
+          </div>          
+        </div>
+
+      </Link>
     </div>
   )
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,41 @@
+"use client"
+import { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import { AiFillHome, AiOutlineMenu } from 'react-icons/ai'
+import { FaUser } from 'react-icons/fa'
+import { ImCancelCircle } from 'react-icons/im'
+import { useState } from 'react'
+
 export const Sidebar: React.FC = () => {
+  const [showSidebar, setShowSidebar] = useState(true);
+
+  const userProfile = false;
+
+  const normalLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold rounded text-[#F51997]';
+
   return (
     <div>
-      Sidebar component
+      <div className='block xl:hidden m-2 ml-4 mt-3 text-xl'
+        onClick={() => setShowSidebar((prev) => !prev)}
+      >
+        {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
+      </div>
+      {showSidebar &&
+        <div className='xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3'>
+          <Link href='/'>
+            <div className={normalLink}>
+              <p className='text-2xl'>
+                <AiFillHome />
+                <span className='text-xl hidden xl:block'>For You</span>
+              </p>
+            </div>
+          </Link>
+          {!userProfile && (
+            <div><p>Login</p></div>
+          )}
+        </div>
+      }
     </div>
   )
 }
